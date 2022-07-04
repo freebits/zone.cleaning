7ee5b1a8088784ceb8bccb8054bbbd8e9d6c42f4
abc def <radicaldata@users.noreply.github.com>
2022-07-04 14:12:27 +1000

    Created landing page draft

--- public/index.php
+++ public/index.php
@@ -4,4 +4,4 @@ require_once dirname(__DIR__).'/vendor/autoload.php';
 $loader = new \Twig\Loader\FilesystemLoader('../views/');
 $twig = new \Twig\Environment($loader, []);
 
-echo $twig->render('index_template.twig');
+echo $twig->render('index_template.php.twig');
